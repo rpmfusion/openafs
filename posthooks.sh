0c4de13892f140463a0bc2c7976418e80837a4ac
Ken Dreyer <ktdreyer@ktdreyer.com>
2012-11-10 00:29:02 +0000

    fix grammar in posthooks script

--- posthooks.sh
+++ posthooks.sh
@@ -10,7 +10,7 @@
 # This script runs as a ExecStartPost from the openafs-client.service
 # systemd unit.  It will source files in /etc/openafs/posthooks.d
 # where an admin may place scripts to fine tune the OpenAFS client
-# operations.  Such as turning on encryption.  The sysnames.sh script
+# operations, such as turning on encryption.  The sysnames.sh script
 # in that directory comes with this package.
 
 HOOKS=/etc/openafs/posthooks.d
